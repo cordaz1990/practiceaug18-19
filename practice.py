89c2080f2fae0cb394fe6028a701009e5eb090a8
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 18:03:03 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,20 @@
-print('hello')
-print('world')
-print('hello')
+>>> s = 'abc'
+>>> t = [0,1,2]
+>>> zip(s, t)
+
+>>> for pair in zip(s, t):
+        print(pair)
+ 
+('a', 0)
+('b', 1)
+('c', 2)
+
+>>> list(zip(s, t))
+[('a', 0), ('b', 1), ('c', 2)]
+
+>>> list(zip('Anne', 'Elk'))
+[('A', 'E'), ('n','l'), ('n', 'k')]
+
+t = [('a', 0), ('b', 1), ('c',2)
+for letter, number in t:
+    print(number, letter)
